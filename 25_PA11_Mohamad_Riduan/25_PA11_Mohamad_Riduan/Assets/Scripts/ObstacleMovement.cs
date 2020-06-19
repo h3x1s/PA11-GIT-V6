d66790a0e1dd60cb9d8b8a90ee2acc34d98782ac
h3x1s <64124310+h3x1s@users.noreply.github.com>
2020-06-20 03:14:58 +0800

    Updated PA11
    Updated by Riduan

---
+++ 25_PA11_Mohamad_Riduan/25_PA11_Mohamad_Riduan/Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    private float xSpeed = -4f;
+    private int Score;
+    public Text txtScore;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtScore = GetComponent<Text>();
+        
+        txtScore.text = "Score:" + Score;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(xSpeed*Time.deltaTime, 0, 0f));
+        if (transform.position.y < -5)
+        {
+            txtScore.text = "Score:" + Score;
+            Destroy(this.gameObject);
+            
+            Score++;
+            
+        }
+    }
+}
---
+++ 25_PA11_Mohamad_Riduan/25_PA11_Mohamad_Riduan/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+      
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.name == "RedCube(Clone)")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("GameOverScene");
+            Debug.Log("Hit");
+        }
+        if(collision.collider.name == "GreenCube(Clone)")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("GameOverScene");
+        }
+        if (collision.collider.name == "BrownCube(Clone)")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+}
